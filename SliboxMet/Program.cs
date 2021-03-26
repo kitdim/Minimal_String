36d7bd0a412ee783ba5b7b5752141748c7ff0f13
Дима <kitaykin9808@gmail.com>
2021-03-26 21:36:56 +0300

    Добавьте файлы проекта.

---
+++ SliboxMet/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SliboxMet
+{
+    class Program
+    {
+        /// <summary>
+        /// Заполнение массива с вводом через клавиатуру
+        /// </summary>
+        /// <param name="arr"></param>
+        static void Fill(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        /// <summary>
+        /// Вывод массива
+        /// </summary>
+        /// <param name="args"></param>
+        static void Input(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+
+        /// <summary>
+        /// Вывод минимальной строки 
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        static string InputString(string[] arr)
+        {
+            string letter = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (letter.Length>arr[i].Length)
+                {
+                    letter = arr[i];
+                    
+                }
+            }
+            return letter;
+        }
+
+        /// <summary>
+        /// Пауза
+        /// </summary>
+        /// <param name="args"></param>
+        static void Delay()
+        {
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            string[] array = new string[5];
+
+            Fill(array); Console.Clear();
+            Input(array);
+
+
+            Console.WriteLine($"\nМинимальная последовательность = {InputString(array)}");
+            Delay();
+        }
+    }
+}
