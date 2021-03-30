e7f70901a9ed87b0cb2f88bd2c2033081a308d8d
Дима <kitaykin9808@gmail.com>
2021-03-30 16:35:28 +0300

    1.0

---
+++ SkillMet2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SkillMet2
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод для очистки повторяющихся значений
+        /// </summary>
+        /// <param name="str"></param>
+        static void Rep(string str)
+        {
+            int i = 0;
+            while (true)
+            {
+                var tmp = str[i].ToString();
+                str = str.Replace(tmp, "");
+                str = str.Insert(i, tmp);
+                i++;
+                if (str.Length - 1 < i) break;
+            }
+            Console.WriteLine(str);
+        }
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            Rep(str);
+            Console.ReadKey();
+        }
+    }
+}
